108ee0c2e7688af2560c091861c207c3eb00e112
newkayak12 <newkayak12@gmail.com>
2021-09-16 17:24:47 +0900

    crane

---
+++ src/programmers/kakaoLv1/craneGame/CraneGame.java
@@ -0,0 +1,53 @@
+package programmers.kakaoLv1.craneGame;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+
+public class CraneGame {
+
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> result = new Stack<>();
+        ArrayList<Stack<Integer>> list = new ArrayList<>(5);
+        for(int i = 0; i<board.length; i++){
+            list.add(new Stack<Integer>());
+        }
+        
+        for(int i = board.length-1; i>=0; i--){
+            for(int j = 0; j<board[i].length; j++){
+                list.get(j).push(board[i][j]);
+            }
+        }
+       
+
+        for(int piece : moves){
+            
+            if(result.isEmpty()){
+                while(list.get(piece-1).peek()!=0){
+                    list.get(piece-1).pop();
+                }
+                result.push(list.get(piece-1).pop());
+            } else {
+                if(result.peek() == list.get(piece-1).peek()){
+                    while(list.get(piece-1).peek()!=0){
+                        list.get(piece-1).pop();
+                    }
+                    result.pop();
+                    list.get(piece-1).pop();
+                    answer++;
+                } else {
+                    while(list.get(piece-1).peek()!=0){
+                        list.get(piece-1).pop();
+                    }
+                    result.push(list.get(piece-1).pop());
+                }
+                
+            } 
+
+            System.out.println(result);
+        }
+        
+        return answer;
+    }
+}
