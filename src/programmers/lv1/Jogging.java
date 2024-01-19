c2547ef22696bbd15f46afcdb2a5c2135a3946ac
newkayak12 <newkayak12@gmail.com>
2024-01-19 23:17:55 +0900

    solve

--- src/programmers/lv1/Jogging.java
+++ src/programmers/lv1/Jogging.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class Jogging {
 
@@ -67,145 +65,245 @@ public class Jogging {
     @Nested
     class TestCases {
         @Test
-        public void case1 () {
-            String[] park = {"SOO","OOO","OOO"};
-            String[] routes = {"E 2","S 2","W 1"};
+        public void case1() {
+            String[] park = {"SOO", "OOO", "OOO"};
+            String[] routes = {"E 2", "S 2", "W 1"};
             int[] result = {2, 1};
 
-            Assertions.assertArrayEquals(result, solution(park,routes));
+            Assertions.assertArrayEquals(result, solution(park, routes));
         }
 
 
         @Test
-        public void case2 () {
-            String[] park = {"SOO","OXX","OOO"};
-            String[] routes = {"E 2","S 2","W 1"};
+        public void case2() {
+            String[] park = {"SOO", "OXX", "OOO"};
+            String[] routes = {"E 2", "S 2", "W 1"};
             int[] result = {0, 1};
 
-            Assertions.assertArrayEquals(result, solution(park,routes));
+            Assertions.assertArrayEquals(result, solution(park, routes));
         }
 
         @Test
-        public void case3 () {
-            String[] park = {"OSO","OOO","OXO","OOO"};
-            String[] routes = {"E 2","S 3","W 1"};
+        public void case3() {
+            String[] park = {"OSO", "OOO", "OXO", "OOO"};
+            String[] routes = {"E 2", "S 3", "W 1"};
             int[] result = {0, 0};
 
-            Assertions.assertArrayEquals(result, solution(park,routes));
+            Assertions.assertArrayEquals(result, solution(park, routes));
         }
+
+        @Test
+        public void case4() {
+            String[] park = {"OSO", "OOO", "O0O", "OOO"};
+            String[] routes = {"E 2", "W 2", "S 4", "N 2"};
+            int[] result = {0, 1};
+
+            Assertions.assertArrayEquals(result, solution(park, routes));
+        }
+
+        @Test
+        public void case5() {
+            String[] park = {"XSX", "XXX", "XXX", "XXX"};
+            String[] routes = {"E 1", "W 1", "S 1", "N 1"};
+            int[] result = {0, 1};
+
+            Assertions.assertArrayEquals(result, solution(park, routes));
+        }
+
+        @Test
+        public void case6() {
+            String[] park = {"XSX", "XXX", "XXX", "XXX"};
+            String[] routes = {"E 2", "W 2", "S 2", "N 2"};
+            int[] result = {0, 1};
+
+            Assertions.assertArrayEquals(result, solution(park, routes));
+        }
+
+        @Test
+        public void case7() {
+            String[] park = {"XSX", "XOX", "XXX", "XOX"};
+            String[] routes = {"E 2", "W 2", "S 3", "S 1"};
+            int[] result = {1, 1};
+
+            Assertions.assertArrayEquals(result, solution(park, routes));
+        }
+
+        @Test
+        public void case8() {
+            String[] park = {"XSX", "XOX", "XOO", "OOX"};
+            String[] routes = {"E 2", "W 2", "S 2", "W 1", "E 1", "W 1", "S 1", "E 1", "W 1"};
+            int[] result = {3, 0};
+
+            Assertions.assertArrayEquals(result, solution(park, routes));
+        }
+
     }
 
+    /**
+     * 정답으로 제출하면 런타임에러만 죄다 남
+     * switchCase쪽인거 같고 ArrayIndexOutOfBoundsException으로 추측됨
+     */
 
-    public int[] solution( String[] park, String[] routes ) {
+    public int[] solution(String[] park, String[] routes) {
         int startX = 0;
         int startY = 0;
 
-        String[][] map = Arrays.stream(park)
-                               .map(elemX -> elemX.split(""))
-                               .toArray(String[][]::new);
+        String[][] map = Arrays.stream(park).map(elemX -> elemX.split("")).toArray(String[][]::new);
 
-        print(map);
 
-        findY: for (; startY < park.length; startY ++ ) {
-            findX: for (; startX < park[0].length(); startX ++ ) {
-                if("S".equals(map[startY][startX])) break findY;
+        findY:
+        for (; startY < park.length; startY++) {
+            findX:
+            for (; startX < park[0].length(); startX++) {
+                if ("S".equals(map[startY][startX])) break findY;
             }
         }
 
 
-
-        System.out.println();
-        System.out.println(startY + ", "+ startX);
-        return new int[] {1,0};
-    }
+        route:
+        for (String route : routes) {
 
 
-    public void print (String[][] map) {
-        for (String[] x : map) {
-            System.out.println(Arrays.stream(x).collect(Collectors.joining("|")));
-        }
-    }
+            char direction = route.charAt(0);
+            Integer distance = Character.getNumericValue(route.charAt(2));
 
 
+            if ('N' == direction || 'W' == direction) distance *= -1;
 
-
-
-
-
-
-
-
-
-    @Deprecated
-    public int[] solutionBak( String[] park, String[] routes ) {
-       String startPoint = "0,0";
-       List<String> obstacles = new ArrayList();
-
-       int xMin = 0;
-       int yMin = 0;
-       int xMax = park[0].length() - 1;
-       int yMax = park.length - 1;
-
-
-       for ( int y = 0; y < park.length; y ++  ) {
-           String[] xLine = park[y].split("");
-           for ( int x = 0; x < xLine.length; x ++ ) {
-               if( xLine[x].equals("X")) obstacles.add(getPosition(y, x));
-               if( xLine[x].equals("S")) startPoint = getPosition(y, x);
-           }
-       }
-
-
-        for (String path : routes) {
-            System.out.println(startPoint);
-            System.out.println(path);
-
-            String[] pathSplit = path.split(" ");
-            Boolean isHorizontal = "E".equals(pathSplit[0]) || "W".equals(pathSplit[0]) ? true : false;
-            Integer distance = "E".equals(pathSplit[0]) || "S".equals(pathSplit[0]) ? Integer.parseInt(pathSplit[1]) : -1 *Integer.parseInt(pathSplit[1]);
-
-            String[] splitPosition = startPoint.split(",");
-            Integer x = Integer.parseInt(splitPosition[1]);
-            Integer y = Integer.parseInt(splitPosition[0]);
-
-
-            if (
-                   (isHorizontal && (x + distance  < xMin ||  x + distance  >  xMax)) ||
-                   (!isHorizontal && (y + distance < yMin || y + distance  >  yMax ))
-            ) {
+            if (('E' == direction || 'W' == direction) && (startX + distance < 0 || (startX + distance) > (map[0].length - 1)))
                 continue;
+            if (('N' == direction || 'S' == direction) && (startY + distance < 0 || (startY + distance) > (map.length - 1)))
+                continue;
+
+            Boolean isBomb = Boolean.FALSE;
+            switch (direction) {
+                case 'E':
+                    for (int i = startX; i <= startX + distance; i++) {
+                        if ("X".equals(map[startY][i])) isBomb = Boolean.TRUE;
+                    }
+                    break;
+                case 'W':
+                    for (int i = startX; i >= startX + distance; i--) {
+                        if ("X".equals(map[startY][i])) isBomb = Boolean.TRUE;
+                    }
+                    break;
+                case 'S':
+                    for (int i = startY; i <= startY + distance; i++) {
+                        if ("X".equals(map[i][startX])) isBomb = Boolean.TRUE;
+                    }
+                    break;
+                case 'N':
+                    for (int i = startY; i >= startY + distance; i--) {
+                        if ("X".equals(map[i][startX])) isBomb = Boolean.TRUE;
+                    }
+                    break;
             }
 
+            if (isBomb) continue;
+            else if (('N' == direction || 'S' == direction)) startY += distance;
+            else startX += distance;
 
-
-            if ( isHorizontal ) {
-                int max = Math.max(x,(x + distance));
-                int min = Math.min(x,(x + distance));
-                Pattern patternX = Pattern.compile(String.format("%d,"+"[\\d]{%d,%d}", y, min, max), Pattern.DOTALL);
-                Optional<String> obstacle = obstacles.stream().filter(elem -> patternX.matcher(elem).find()).findAny();
-                if ( obstacle.isPresent() ) continue;
-                startPoint = getPosition( y, x + distance);
-            } else {
-                int max = Math.max(y, (y + distance));
-                int min = Math.min(y, (y + distance));
-                Pattern patternY = Pattern.compile(String.format("[\\d]{%d,%d},%d", min, max, x), Pattern.DOTALL);
-                Optional<String> obstacle = obstacles.stream().filter(elem -> patternY.matcher(elem).find()).findAny();
-                if ( obstacle.isPresent() ) continue;
-                startPoint = getPosition( y + distance, x);
-            }
-
-
-//            System.out.println(String.format("-----------%s------------", path));
-//            System.out.println(startPoint);
-//            System.out.println("---------------------------\n");
+            System.out.println(route);
+            System.out.println(startY + "," + startX);
+            System.out.println();
         }
 
-        return Arrays.stream(startPoint.split(",")).mapToInt(Integer::parseInt).toArray();
-    }
-    private String getPosition( Integer y, Integer x ) {
-        return String.format("%d,%d", y, x);
+        return new int[]{startY, startX};
     }
 
 
+    /**
+     * 다른 사람 풀인데 무슨 차이인지 모르겠음
+     *
+     */
+    class Solution {
+        public int[] solution(String[] park, String[] routes) {
 
+            // 이차원 배열을 새로 만들면서, 로봇의 시작 위치를 찾는다.
+            int robotRow = -1;
+            int robotColumn = -1;
+
+            int rowNum = park.length;
+            int columnNum = park[0].length();
+
+            char[][] parkArray = new char[rowNum][columnNum];
+
+            for (int i = 0; i < rowNum; i++) { //50
+                String rowData = park[i];
+                for (int j = 0; j < columnNum; j++) { //50
+
+                    char locationData = rowData.charAt(j);
+                    parkArray[i][j] = locationData;
+
+                    if (locationData == 'S') {
+                        robotRow = i;
+                        robotColumn = j;
+                    }
+                }
+            }
+
+
+            // 좌표 값 저장
+            Map<Character, List<Integer>> dirMove = new HashMap<Character, List<Integer>>();
+            dirMove.put('E', List.of(0, 1));
+            dirMove.put('W', List.of(0, -1));
+            dirMove.put('N', List.of(-1, 0));
+            dirMove.put('S', List.of(1, 0));
+
+
+            // routes 만큼 반복한다.
+            for (String route : routes) {
+
+                char direction = route.charAt(0);
+                Integer move = route.charAt(2) - '0';
+
+
+                // 이동할 위치 계산
+                // 현재 위치를 먼저 저장한다. (현재 명령이 불가능하다면 롤백을 해주기 위함)
+                int tmpRow = robotRow;
+                int tmpColumn = robotColumn;
+
+                Boolean possible = true;
+
+                // 해당 방향으로 몇칸을 이동해야해서, 한칸씩 이동하며 가능성을 확인한다.
+                for (int i = 0; i < move; i++) {
+
+                    Integer moveRow = robotRow + dirMove.get(direction).get(0);
+                    Integer moveColumn = robotColumn + dirMove.get(direction).get(1);
+
+                    // 공원 바깥이면 실패
+                    if (0 > moveRow || moveRow >= rowNum || 0 > moveColumn || moveColumn >= columnNum) {
+                        possible = false;
+                        break;
+                    }
+
+                    // 장애물을 만나면 실패
+                    if (parkArray[moveRow][moveColumn] == 'X') {
+                        possible = false;
+                        break;
+                    }
+
+                    //실제 이동
+                    parkArray[moveRow][moveColumn] = 'S';
+                    parkArray[robotRow][robotColumn] = 'O';
+
+                    robotRow = moveRow;
+                    robotColumn = moveColumn;
+                }
+
+                //불가능한 경우가 존재하면 롤백
+                if (possible == false) {
+                    robotRow = tmpRow;
+                    robotColumn = tmpColumn;
+                }
+            }
+
+            // 마지막에 로봇의 위치를 배열에 저장해서 return하면 끝
+            int[] answer = new int[2];
+            answer[0] = robotRow;
+            answer[1] = robotColumn;
+            return answer;
+        }
+
+    }
 }
