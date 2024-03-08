0b69f5d2981cb0eba9c949274a8564660c0744c7
newkayak12 <newkayak12@gmail.com>
2024-03-08 23:43:46 +0900

    Hmm...

--- src/programmers/lv2/ContinuousSequential.java
+++ src/programmers/lv2/ContinuousSequential.java
@@ -41,16 +41,19 @@ public class ContinuousSequential {
     }
 
     public int solution ( int [] elements ) {
-        Set<Integer> sumCase = new HashSet<>();
-        int length = elements.length;
 
-
-
-
-
-        System.out.println(sumCase);
-
-        return 0;
+        Set<Integer> result = new HashSet<>();
+        for ( int i = 1; i <= elements.length; i ++ ) {
+            for ( int j = 0; j < elements.length; j ++ ) {
+                List<Integer> sub = new ArrayList<>();
+                for ( int l = j;  l < j + i; l ++ ) {
+                    if( l <= elements.length  - 1) sub.add(elements[l]);
+                    else sub.add(elements[l - ( elements.length)] );
+                }
+                result.add(sub.stream().reduce(0, (p,n) -> p + n));
+            }
+        }
+        return result.size();
     }
 
     public int solutionFailure ( int [] elements ) {
