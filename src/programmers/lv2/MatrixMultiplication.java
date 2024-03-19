060d9e17cc48a4ad09f2f1f53897dfe0c5fcf2f9
newkayak12 <newkayak12@gmail.com>
2024-03-19 22:40:57 +0900

    hashing

--- src/programmers/lv2/MatrixMultiplication.java
+++ src/programmers/lv2/MatrixMultiplication.java
@@ -123,8 +123,8 @@ public class MatrixMultiplication {
 
         int[][] answer = new int[arr1.length][arr2[0].length];
 
-        for( int i = 0; i < arr1.length; i ++ ) {
-            for (int j = 0; j < arr2[0].length; j ++ ) {
+        for( int i = 0; i < answer.length; i ++ ) {
+            for (int j = 0; j < answer[0].length; j ++ ) {
                 int tmp = 0;
 
                 for( int k = 0; k < arr2.length; k++ ) {
