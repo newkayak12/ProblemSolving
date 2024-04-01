12e2524a49ee42b1cdb223aa7ba8ccbcfb938737
newkayak12 <newkayak12@gmail.com>
2024-04-02 07:16:04 +0900

    solv

--- src/programmers/lv2/kakaoLv2/Compress.java
+++ src/programmers/lv2/kakaoLv2/Compress.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -70,7 +69,7 @@ public class Compress {
         @Test
         void case3 () {
             String msg = "ABABABABABABABAB";
-            int[] answer = new int[]{1, 2, 27, 29, 28, 31, 3};
+            int[] answer = new int[]{1, 2, 27, 29, 28, 31, 30};
             Assertions.assertArrayEquals(answer, solution(msg));
         }
     }
@@ -103,9 +102,29 @@ public class Compress {
             } else {
                 tmp = now+next;
             }
+
         }
 
-        list.add(dictionary.indexOf(String.valueOf(msg.charAt(msg.length() - 1))) + 1);
+
+
+        String result = String.valueOf(msg.charAt(msg.length() - 1));
+        if( !tmp.isEmpty() ) result  = tmp;
+        list.add(dictionary.indexOf(result) + 1);
+
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
+
+/**
+ * 해설
+ *
+ * 어피치는 여러 압축 알고리즘 중에서 성능이 좋고 구현이 간단한 LZW(Lempel–Ziv–Welch) 압축을 구현하기로 했다. LZW 압축은 1983년 발표된 알고리즘으로, 이미지 파일 포맷인 GIF 등 다양한 응용에서 사용되었다.
+ *
+ * LZW 압축은 다음 과정을 거친다.
+ *
+ * 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
+ * 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
+ * w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
+ * 입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
+ * 단계 2로 돌아간다.
+ */
