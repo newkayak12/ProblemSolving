574a2f53110ee43708e0f37a090bdbefdc0f0759
newkayak12 <newkayak12@gmail.com>
2024-06-29 19:09:48 +0900

    naw

--- src/programmers/lv1/kakaoLv1/wordAndEng/WordAndEng.java
+++ src/programmers/lv1/kakaoLv1/wordAndEng/WordAndEng.java
@@ -8,6 +8,9 @@ import java.util.Map;
 
 public class WordAndEng {
     /**
+     * <pre>
+     * 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+     *
      * 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
      * 1478 → "one4seveneight"
      * 234567 → "23four5six7"
@@ -25,21 +28,15 @@ public class WordAndEng {
      * 7	seven
      * 8	eight
      * 9	nine
+     *
+     * -  1 ≤ s의 길이 ≤ 50
+     * -  s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
+     * -  return 값이 1 이상 2,000,000,000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
+     * </pre>
      */
 
 
-    private static final Map<String, Integer> mapping = Map.of(
-            	"zero",0,
-            	"one",1,
-            	"two",2,
-            	"three",3,
-            	"four",4,
-            	"five",5,
-            	"six",6,
-            	"seven",7,
-            	"eight",8,
-            	"nine",9
-    );
+
 
     @Nested
     class TestCases {
@@ -71,11 +68,49 @@ public class WordAndEng {
         }
     }
 
-    public static int solution( String s ) {
-        String answer = new String(s);
-        for ( String key : mapping.keySet() ) {
-            answer = answer.replaceAll(key, String.valueOf(mapping.get(key)));
-        };
-        return Integer.parseInt(answer);
+
+    public  int solution( String s ) {
+        Map<String, String> map = Map.of(
+                "zero","0",
+                "one","1",
+                "two","2",
+                "three","3",
+                "four","4",
+                "five","5",
+                "six","6",
+                "seven","7",
+                "eight","8",
+                "nine","9"
+        );
+
+        for( String key : map.keySet() ) {
+            s = s.replaceAll(key, map.get(key));
+        }
+
+        return Integer.parseInt(s);
     }
+
+    public class Success {
+
+        private static final Map<String, Integer> mapping = Map.of(
+                "zero",0,
+                "one",1,
+                "two",2,
+                "three",3,
+                "four",4,
+                "five",5,
+                "six",6,
+                "seven",7,
+                "eight",8,
+                "nine",9
+        );
+        public static int solution( String s ) {
+            String answer = new String(s);
+            for ( String key : mapping.keySet() ) {
+                answer = answer.replaceAll(key, String.valueOf(mapping.get(key)));
+            };
+            return Integer.parseInt(answer);
+        }
+    }
+
 }
